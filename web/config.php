f86df6e051da89dbd3918f62928af9f03c0c079f
maoxt <27711210@qq.com>
2018-03-01 00:13:43 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', getenv("AIzaSyB_iLofCCwB9JfwQfLO709JTkMNu9dOyY4"));
+define('GJ_CODE', getenv("TW"));
+define('SITE_NAME',getenv("MYWEB"));
+define('TITLENAME', getenv("YOU2PHP"));
+define('EN2DEKEY',getenv("66666668888888"));
+define('EMAIL', getenv("danger_dego@hotmail.com"));
 ?>
